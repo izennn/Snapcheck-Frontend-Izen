dc4b02625e7771a57dac7794b7b1f8875539d30f
izen.oku <izenhuang@hotmail.com>
2020-10-23 01:22:12 -0700

    feat: added browser router and history

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch, Redirect, useHistory } from 'react-router-dom';
 
 // components
+import PrivateRoute from './components/PrivateRoute';
+import Navbar from './components/Navbar';
 import Login from './components/Login';
+import Users from './components/Users';
+import EditUser from './components/Users';
+// context
+import UserContextProvider from './contexts/UserContext';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const history = useHistory();
+
+	useEffect(() => {
+		history.push('/login')
+	})
 
   return (
     <div 
@@ -12,18 +24,39 @@ function App() {
       style={{ 
         width: '100vw', 
         height: '100vh', 
-        padding: '3em 10em 0 10em',
-        border: '1px solid red', 
+        padding: '0em 10em 0 10em',
       }}
     >
-      { !isLoggedIn ? 
-        <Login 
-          setIsLoggedIn={setIsLoggedIn}
-        />
-        : 
-        <p>Is logged in!</p>
-      }
-    </div>
+				<UserContextProvider>
+					<div>
+						<Navbar />
+						<Switch>
+							<PrivateRoute 
+								exact 
+								path="/users" 
+								isLoggedIn={isLoggedIn} 
+								component={Users} 
+							/>
+							<PrivateRoute 
+								exact 
+								path="/users/:userId" 
+								isLoggedIn={isLoggedIn} 
+								component={EditUser} 
+							/>
+							<Route 
+								exact 
+								path="/login" 
+								component={() => 
+									!isLoggedIn
+									? <Login setIsLoggedIn={setIsLoggedIn} />  
+									: <Redirect to='/users' />
+								}
+							/>
+							<Redirect to="/login" />
+						</Switch>
+					</div>
+				</UserContextProvider>			
+		</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // styling
 import 'semantic-ui-css/semantic.min.css';
@@ -8,7 +9,9 @@ import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
 
