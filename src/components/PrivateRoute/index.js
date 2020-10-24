6ddc0f75944895d1e3f4acd2313aa2d26341b4a4
izen.oku <izenhuang@hotmail.com>
2020-10-23 23:42:43 -0700

    clean: removed unnecessary console.log

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({component: Component, isLoggedIn, ...rest}) => {
-	console.log("Got to a private route")
-	console.log("Is logged in? " + isLoggedIn)
 	return (
 		<Route
 			{...rest}
