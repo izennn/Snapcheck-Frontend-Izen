b519cf74336591e6dbae0391e7a68dfd6f9ca383
izen.oku <izenhuang@hotmail.com>
2020-10-23 23:42:57 -0700

    feat: address pipe

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -6,15 +6,14 @@ import { backendBaseUrl } from '../../shared/hostnames';
 // styling
 import { Container, Input, Table } from 'semantic-ui-react';
 
-// TODO:
+// TODO
 // 1. add link for each table row to individual user
-// 2. "amount pipe"
-// 3. "currency pipe"
-// 4. Search bar
+// 4. Search bar: could search by firstname, lastname, or amount
 
 const Users = () => {
-	const [users, setUsers] = useState([])
-	const [searchPhrase, setSearchPhrase] = useState('');
+	const [users, setUsers] = useState([]);
+	const [displayOrders, setDisplayOrders] = useState([]);
+	const searchPhrase = useFormInput('');
 
 	useEffect(() => {
 		// fetch all users
@@ -23,33 +22,56 @@ const Users = () => {
 				if (res.data) {
 					setUsers(res.data)
 				} else {
-					console.log("Error in response")
+					console.log("Error in backend/orders response")
 				}
 			})
 			.catch((err) => console.log(err))
 	}, [])
 
+	useEffect(() => {
+		setDisplayOrders(users);
+	}, [users])
+
+	useEffect(() => {
+		// do something to displayOrders when search phrase is changing
+	}, [searchPhrase])
+
 	return (
-		<Container>
-			<Input focus placeholder="Search name, address ..." />
+		<Container style={{overflowY: 'auto'}}>
+			<Input 
+				focus 
+				placeholder="Search name, address ..." 
+				{...searchPhrase}
+			/>
 			<Table celled>
 				<Table.Header>
 					<Table.Row>
 						<Table.HeaderCell>Username</Table.HeaderCell>
 						<Table.HeaderCell>Address</Table.HeaderCell>
 						<Table.HeaderCell>Gender</Table.HeaderCell>
-						<Table.HeaderCell>Order Total</Table.HeaderCell>
+						<Table.HeaderCell>Age</Table.HeaderCell>
+						<Table.HeaderCell 
+							style={{
+								textAlign: 'right'
+							}}
+						>
+							Amount
+						</Table.HeaderCell>
 					</Table.Row>
 				</Table.Header>
 				<Table.Body>
 				{
-					users.length > 0 ? users.map((user, userId) => {
+					displayOrders.length > 0 ? displayOrders.map((user, userId) => {
 						return (
-							<Table.Row key={userId}>
-								<Table.Cell>{user.first_name}&nbsp;{user.last_name}</Table.Cell>
-								<Table.Cell>{user.address.address1}</Table.Cell>
-								<Table.Cell>{user.gender}</Table.Cell>
-								<Table.Cell>{user.order_total.amount}</Table.Cell>
+							<Table.Row 
+								key={userId}
+								onClick={() => console.log(userId)}
+							>
+								<Table.Cell collapsing>{user.first_name}&nbsp;{user.last_name}</Table.Cell>
+								<Table.Cell>{addressPipe(user.address)}</Table.Cell>
+								<Table.Cell collapsing>{user.gender}</Table.Cell>
+								<Table.Cell collapsing>{user.age}</Table.Cell>
+								<Table.Cell textAlign='right' collapsing>{amountPipe(user.order_total)}</Table.Cell>
 							</Table.Row>
 						)
 					}) : null
@@ -58,6 +80,49 @@ const Users = () => {
 			</Table>	
 		</Container>
 	)
+
+	function useFormInput(initialValue) {
+		const [value, setValue] = useState(initialValue);
+		function handleChange(e) {
+			setValue(e.target.value);
+		}
+
+		return {
+			value, 
+			onChange: handleChange
+		};
+	}	
+
+	// given address, return formatted string
+	function addressPipe(address) {
+		const { address1, address2, city, state, zip } = address;
+
+		let returnString = `${address1} ${address2} ${city}, ${state} ${zip}`;
+		return returnString;
+	}
+
+	// given order_total object, return formatted amount i.e. currency symbol and dec
+	function amountPipe(order_total) {
+		if (order_total === null || order_total.amount === undefined) {
+			return "Null";
+		}
+
+		let amountString = order_total.amount.toString();
+		let amountStringLen = amountString.length;
+		let currency = order_total.currency;
+
+		if (amountStringLen === 1) {
+			amountString = `0.0${amountString}`;
+		} else if (amountStringLen === 2) {
+			amountString = `0.${amountString}`;
+		} else {
+			let dotIndex = amountStringLen - 2;
+			let temp = amountString.slice(0, dotIndex) + '.' + amountString.slice(dotIndex);
+			amountString = temp;
+		}
+
+		return `${amountString} ${currency}`;
+	}
 };
 
 export default Users;
