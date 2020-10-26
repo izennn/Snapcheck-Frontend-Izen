41af535b94922791071af40814ffb134a23ef769
izen.oku <izenhuang@hotmail.com>
2020-10-25 21:56:54 -0700

    fix: capitalized constants

--- src/shared/hostnames.js
+++ src/shared/hostnames.js
@@ -1,2 +1,2 @@
-export const backendBaseUrl = 'http://localhost:4000';
-export const frontendBaseUrl = 'http://localhost:3000';
+export const BACKEND_BASE_URL = 'http://localhost:4000';
+export const FRONTEND_BASE_URL = 'http://localhost:3000';
